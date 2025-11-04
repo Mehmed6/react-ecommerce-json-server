6bb4a1898c3996efd6cf1368d0e2c34791af73b3
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-11-04 23:50:36 +0100

    Add TypeScript interfaces: UserType, ProductType, RatingType
    - Created UserType interface for user data
    - Created ProductType interface for product data
    - Added RatingType interface for product ratings
    - Centralized all types in Types.tsx for better type safety

---
+++ src/types/Types.tsx
@@ -0,0 +1,22 @@
+export interface UserType {
+    id: string;
+    username: string;
+    password: string;
+    balance: number
+}
+
+export interface ProductType {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    count?: number,
+    image: string,
+    rating: RatingType
+}
+
+interface RatingType {
+    rate: number,
+    count: number
+}
